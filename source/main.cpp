ee3a8cbdc075f56c38be7e0135e81f29cb9337ab
Mikhail Chalakov <mikhail@chalakov.net>
2021-06-10 23:07:13 -0700

    made cmake work like a beast, todo: fix xwindow glitch with resizing scale, add characters as well

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,12 @@
 //
 // Created by mikhail on 5/20/21.
 //
-// #include "../include/ResourceLoader.h"
-// #include "../include/Game.h"
-// #include "../include/WindowManager.h"
-// #include "../include/Types.h"
+#include "../include/ResourceLoader.h"
+#include "../include/Game.h"
+#include "../include/WindowManager.h"
+#include "../include/Types.h"
 #include <SDL2/SDL.h>
-// #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 #include <string>
 
@@ -14,15 +14,15 @@ int main()
 {
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    // IMG_Init(IMG_INIT_JPG);
+    IMG_Init(IMG_INIT_JPG);
 
-    // WindowManager window("Boxing", type::Vector2i(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED), SDL_WINDOW_FULLSCREEN);
-    // window.setBackground(ResourceLoader::getInstance()->loadTexture(std::string("../assets/Ring.jpg"), window.getRenderer()));
+    WindowManager window("Boxing", type::Vector2i(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED), SDL_WINDOW_FULLSCREEN);
+    window.setBackground(ResourceLoader::getInstance()->loadTexture(std::string("../assets/Ring.jpg"), window.getRenderer()));
     bool done = false;
 
     while (!done)
     {
-        // if (window.update())
+        if (window.update())
         {
             done = true;
         }
